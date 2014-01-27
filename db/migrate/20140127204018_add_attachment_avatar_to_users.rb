20c70744a0e7bb5ab4252ae25b3c7ad862f0e1d5
Timur Kalimov <tkalimov@gmail.com>
2014-01-27 16:48:02 -0500

    one more tweak to migration

--- db/migrate/20140127204018_add_attachment_avatar_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddAvatarToFriends < ActiveRecord::Migration
-  def self.up
-    add_attachment :friends, :avatar
-  end
-
-  def self.down
-    remove_attachment :friends, :avatar
-  end
-end
