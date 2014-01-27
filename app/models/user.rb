ccba943746b3e97cbb9e9a9a47fdd8752dc0d209
Timur Kalimov <tkalimov@gmail.com>
2014-01-27 16:55:02 -0500

    fix attr_accssible

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 	has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent:   :destroy
   	has_many :followers, through: :reverse_relationships, source: :follower
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-	attr_accessible :name, :email, :password, :password_confirmation
+	attr_accessible :name, :email, :password, :password_confirmation, :avatar
   	has_secure_password				
 	
 	before_save { |user| user.email = email.downcase }
